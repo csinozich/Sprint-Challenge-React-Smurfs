ece00cdd2205a7b5c3c21ba24350da68715e7ce9
Claire Sinozich <claire.sinozich@gmail.com>
2019-04-06 10:22:48 -0600

    mvp complete

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -12,6 +12,7 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
+    this.props.addToServer(this.props)
     // add code to create the smurf using the api
 
     this.setState({
